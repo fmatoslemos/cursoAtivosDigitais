d22dad4a3dcaca62b671b6bc64b35d52657dc672
fmatoslemos <fmatoslemos@gmail.com>
2024-02-08 20:44:16 -0300

    "Entrga do exercício da aula 8"

--- nuclea_aula7/debenture.sol
+++ nuclea_aula7/debenture.sol
@@ -3,13 +3,16 @@ pragma solidity 0.8.19;
 
 import "./owner.sol";
 import "./titulo.sol";
+import "./tokenrc20.sol";
+
+// Endereço do contrato debenture RC20 : 0x4ff9d29c5c3f2e973d8948f27bd51bb2593d9ac0
 
 /**
  * @title Debenture
  * @dev Operacoes de uma debenture
  * @author Jeff Prestes
  */
- contract Debenture is Titulo, Owner {
+ contract Debenture is Titulo, Owner, IERC20 {
 
     string _emissor;
     uint256 immutable _dataEmissao;
@@ -18,9 +21,11 @@ import "./titulo.sol";
     uint256 _prazoPagamento;
     uint16 _fracoes;
     string public rating;
+    string private myName;
+    string private mySymbol;
 
 
-    constructor(string memory emissor_) {
+    constructor(string memory emissor_, string memory myName_, string memory mySymbol_) {
         _emissor = emissor_;
         _dataEmissao = block.timestamp;
         _valor = 100000000;
@@ -28,7 +33,10 @@ import "./titulo.sol";
         _prazoPagamento = _dataEmissao + 90 days;
         rating = "BBB-";
         _fracoes = 1000;
+        myName = myName_;
+        mySymbol = mySymbol_;
         emit NovoPrazoPagamento(_dataEmissao, _prazoPagamento);
+        mint(msg.sender, _valor);
     }
 
     /**
@@ -74,6 +82,87 @@ import "./titulo.sol";
     */
     function fracoes() external view returns (uint16) {
         return _fracoes;
+    }
+
+    mapping (address=>uint256) balances;
+    mapping (address=>mapping (address=>uint256)) ownerAllowances;
+
+    modifier hasEnoughBalance(address owner, uint amount) {
+        uint balance;
+        balance = balances[owner];
+        require (balance >= amount); 
+        _;
+    }
+
+    modifier isAllowed(address spender, address tokenOwner, uint amount) {
+        require (amount <= ownerAllowances[tokenOwner][spender]);
+        _;
+    }
+
+    modifier tokenAmountValid(uint256 amount) {
+        require(amount > 0);
+        require(amount <= _valor);
+        _;
+    }
+
+    function name() public view returns(string memory) {
+        return myName;
+    }
+
+    function symbol() public view returns(string memory) {
+        return mySymbol;
+    }
+
+    function totalSupply() public override view returns(uint256) {
+        return _valor;
+    }
+
+    function balanceOf(address tokenOwner) public override view returns(uint256) {
+        return balances[tokenOwner];
+    }
+
+    function allowance(address tokenOwner, address spender) public override view returns(uint256) {
+        return ownerAllowances[tokenOwner][spender];
+    }
+
+    function transfer(address to, uint256 amount) public override  hasEnoughBalance(msg.sender, amount) tokenAmountValid(amount) returns(bool) {
+        balances[msg.sender] = balances[msg.sender] - amount;
+        balances[to] = balances[to] + amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    } 
+
+    function approve(address spender, uint limit) public override returns(bool) {
+        ownerAllowances[msg.sender][spender] = limit;
+        emit Approval(msg.sender, spender, limit);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public override 
+    hasEnoughBalance(from, amount) isAllowed(msg.sender, from, amount) tokenAmountValid(amount)
+    returns(bool) {
+        balances[from] = balances[from] - amount;
+        balances[to] += amount;
+        ownerAllowances[from][msg.sender] = ownerAllowances[from][msg.sender] - amount;
+        emit Transfer(from, to, amount);
+        return true;
+    }
+    
+    function mint(address account, uint256 amount) public onlyOwner returns (bool) {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _valor = _valor + amount;
+        balances[account] = balances[account] + amount;
+        emit Transfer(address(0), account, amount);
+        return true;
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner returns (bool) {
+        require(account != address(0), "ERC20: burn from address");
+        balances[account] = balances[account] - amount;
+        _valor = _valor - amount;
+        emit Transfer(account, address(0), amount);
+        return true;
     }
 
  }
